09577bed0700b8e2ca22bdcd9db1a92bd6bbe711
David G <david.eve.za@gmail.com>
2023-02-14 19:15:43 -0600

    Add Cache to DB Calls

--- src/main/java/com/devlopment/productservice/services/ProductServiceImpl.java
+++ src/main/java/com/devlopment/productservice/services/ProductServiceImpl.java
@@ -2,6 +2,9 @@ package com.devlopment.productservice.services;
 
 import com.devlopment.productservice.models.entitys.Product;
 import com.devlopment.productservice.models.repository.IProductRepository;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,23 +20,33 @@ public class ProductServiceImpl implements IProductService{
 
     @Override
     @Transactional(readOnly = true)
+    @Cacheable("products")
     public List<Product> findAll() {
         return productRepository.findAll();
     }
 
     @Override
     @Transactional(readOnly = true)
+    @Cacheable(value = "product")
     public Product findById(Long id) {
         return productRepository.findById(id).orElse(null);
     }
 
     @Override
     @Transactional(readOnly = false)
+    @Caching(evict = {
+            @CacheEvict(value = "products"),
+            @CacheEvict(value = "product")
+    })
     public Product save(Product product) {
         return productRepository.save(product);
     }
 
     @Override
+    @Caching(evict = {
+            @CacheEvict(value = "products"),
+            @CacheEvict(value = "product")
+    })
     public void delete(Long id) {
         productRepository.deleteById(id);
     }
